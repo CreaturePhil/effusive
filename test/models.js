ec825da2281d502cc5d03def93d7872dcf9cf22f
Phil <birkal@outlook.com>
2014-10-09 21:55:47 -0700

    Change and merge tests

--- test/models.js
+++ test/models.js
@@ -2,6 +2,10 @@ var chai = require('chai');
 var moment = require('moment');
 var should = chai.should();
 var User = require('../app/models/User');
+var Post = require('../app/models/Post');
+var secrets = require('../config/secrets');
+var Hashids = require('hashids');
+var hashids = new Hashids(secrets.hashidSecret);
 
 describe('user model', function() {
   it('should create a new user', function(done) {
@@ -59,8 +63,54 @@ describe('user model', function() {
     });
   });
 
+  it('should used user\'s methods', function(done) {
+    User.findOne({ email: 'test@gmail.com' }, function(err, user) {
+      if (err) return done(err);
+      user.gravatar();
+      user.comparePassword('password', function(err, isMatch) {
+        isMatch.should.equal(true);
+        done();
+      });
+    });
+  });
+
   it('should delete a user', function(done) {
     User.remove({ email: 'test@gmail.com' }, function(err) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
+
+describe('post model', function() {
+  var post = new Post({
+    turl: 'my-title',
+    title: 'My title',
+    avatar: 'picture.png',
+    author: 'admin',
+    date: moment(),
+    body: 'awesome content body here'
+  });
+
+  it('should create a new post', function(done) {
+    post.save(function(err, savePost) {
+      if (err) return done(err);
+      post = savePost;
+      done();
+    });
+  });
+
+  it('should find post by id', function(done) {;
+    done();
+  });
+
+  it('equal id of object when decoding post hashid', function(done) {
+    hashids.decodeHex(post.getHash()).should.equal(post.id); 
+    done();
+  });
+
+  it('should delete a post', function(done) {
+    Post.remove({ id: post.id }, function(err) {
       if (err) return done(err);
       done();
     });
